4c528f65abc276abadbe5c8b53cbb9c27b0e1ef4
Ted Driggs <ted.driggs@outlook.com>
2019-02-13 10:29:08 -0800

    Preserve outer attributes from files when inlining
    Fixes #1

--- src/lib.rs
+++ src/lib.rs
@@ -77,4 +77,121 @@ mod tests {
             .to_string()
         );
     }
+
+    /// Test case involving `cfg_attr` from the original request for implementation.
+    ///
+    /// Right now, this test fails for two reasons:
+    ///
+    /// 1. We don't look for `cfg_attr` elements
+    /// 2. We don't have a way to insert new items
+    ///
+    /// The first fix is simpler, but the second one would be difficult.
+    #[test]
+    #[should_panic]
+    fn cfg_attrs() {
+        let mut env = TestResolver::default();
+        env.register(
+            "src/lib.rs",
+            r#"
+            #[cfg(feature = "m1")]
+            mod m1;
+
+            #[cfg_attr(feature = "m2", path = "m2.rs")]
+            #[cfg_attr(not(feature = "m2"), path = "empty.rs")]
+            mod placeholder;
+        "#,
+        );
+        env.register("src/m1.rs", "struct M1;");
+        env.register(
+            "src/m2.rs",
+            "
+        //! module level doc comment
+
+        struct M2;
+        ",
+        );
+        env.register("src/empty.rs", "");
+
+        let mut visitor = Visitor::with_resolver(&Path::new("src/lib.rs"), Cow::Borrowed(&env));
+
+        assert_eq!(
+            visitor.visit().into_token_stream().to_string(),
+            quote! {
+                #[cfg(feature = "m1")]
+                mod m1 {
+                    struct M1;
+                }
+
+                #[cfg(feature = "m2")]
+                mod placeholder {
+                    //! module level doc comment
+
+                    struct M2;
+                }
+
+                #[cfg(not(feature = "m2"))]
+                mod placeholder {
+
+                }
+            }
+            .to_string()
+        )
+    }
+
+    #[test]
+    fn cfg_attrs_revised() {
+        let mut env = TestResolver::default();
+        env.register(
+            "src/lib.rs",
+            r#"
+            #[cfg(feature = "m1")]
+            mod m1;
+
+            #[cfg(feature = "m2")]
+            #[path = "m2.rs"]
+            mod placeholder;
+
+            #[cfg(not(feature = "m2"))]
+            #[path = "empty.rs"]
+            mod placeholder;
+        "#,
+        );
+        env.register("src/m1.rs", "struct M1;");
+        env.register(
+            "src/m2.rs",
+            r#"
+            #![doc = " module level doc comment"]
+
+            struct M2;
+            "#,
+        );
+        env.register("src/empty.rs", "");
+
+        let mut visitor = Visitor::with_resolver(&Path::new("src/lib.rs"), Cow::Borrowed(&env));
+
+        assert_eq!(
+            visitor.visit().into_token_stream().to_string(),
+            quote! {
+                #[cfg(feature = "m1")]
+                mod m1 {
+                    struct M1;
+                }
+
+                #[cfg(feature = "m2")]
+                #[path = "m2.rs"]
+                mod placeholder {
+                    #![doc = " module level doc comment"]
+
+                    struct M2;
+                }
+
+                #[cfg(not(feature = "m2"))]
+                #[path = "empty.rs"]
+                mod placeholder {
+
+                }
+            }
+            .to_string()
+        )
+    }
 }
