f9c646a3c35b668482d352ac8e9677213e6f31f2
jethrogb <github@jbeekman.nl>
2019-06-19 02:02:05 +0200

    Distinguish root and non-root modules, detect non-inlined modules (#6)
    * Distinguish root and non-root modules, lib.rs and main.rs are not special
    
    * Add an option to detect non-inlined modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! Utility to traverse the file-system and inline modules that are declared as references to
 //! other Rust files.
 
+use std::{borrow::Cow, path::{Path, PathBuf}};
+
 mod mod_path;
 mod resolver;
 mod visitor;
@@ -11,18 +13,113 @@ pub(crate) use visitor::Visitor;
 
 /// Parse the source code in `src_file` and return a `syn::File` that has all modules
 /// recursively inlined.
+///
+/// This is equivalent to using an `InlinerBuilder` with the default settings.
 pub fn parse_and_inline_modules(src_file: &std::path::Path) -> syn::File {
-    Visitor::<FsResolver>::new(src_file).visit()
+    InlinerBuilder::default().parse_and_inline_modules(src_file).unwrap()
+}
+
+/// A builder that can configure how to inline modules.
+///
+/// After creating a builder, set configuration options using the methods
+/// taking `&mut self`, then parse and inline one or more files using
+/// `parse_and_inline_modules`.
+#[derive(Debug)]
+pub struct InlinerBuilder {
+    root: bool,
+    error_not_found: bool,
+}
+
+impl Default for InlinerBuilder {
+    fn default() -> Self {
+        InlinerBuilder {
+            root: true,
+            error_not_found: false,
+        }
+    }
+}
+
+impl InlinerBuilder {
+    /// Create a new `InlinerBuilder` with the default options.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Configures whether the module being parsed is a root module or not.
+    ///
+    /// A root module is one that is passed directly to `rustc`. A non-root
+    /// module is one that is included from another module using a `mod` item.
+    ///
+    /// Default: `true`.
+    pub fn root(&mut self, root: bool) -> &mut Self {
+        self.root = root;
+        self
+    }
+
+    /// Configures whether unexpanded modules (due to a missing file) will lead
+    /// to an `Err` return value or not.
+    ///
+    /// Default: `false`.
+    pub fn error_not_found(&mut self, error_not_found: bool) -> &mut Self {
+        self.error_not_found = error_not_found;
+        self
+    }
+
+    /// Parse the source code in `src_file` and return a `syn::File` that has all modules
+    /// recursively inlined.
+    pub fn parse_and_inline_modules(&self, src_file: &std::path::Path) -> Result<syn::File, Error> {
+        self.parse_internal(src_file, FsResolver::default())
+    }
+
+    fn parse_internal<R: FileResolver + Clone>(&self, src_file: &std::path::Path, resolver: R) -> Result<syn::File, Error> {
+        let mut errors = if self.error_not_found {
+            Some(vec![])
+        } else {
+            None
+        };
+        let result = Visitor::<R>::with_resolver(src_file, self.root, errors.as_mut(), Cow::Owned(resolver)).visit();
+        match errors {
+            Some(ref errors) if errors.len() == 0 => Ok(result),
+            None => Ok(result),
+            Some(errors) => Err(Error::NotFound(errors)),
+        }
+    }
+}
+
+/// A source location
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SourceLocation {
+    pub path: PathBuf,
+    pub line: usize,
+    pub column: usize,
+    _private: ()
+}
+
+impl SourceLocation {
+    pub(crate) fn new(path: &Path, span: proc_macro2::Span) -> Self {
+        SourceLocation {
+            path: path.into(),
+            line: span.start().line,
+            column: span.start().column,
+            _private: (),
+        }
+    }
+}
+
+/// An error that was encountered while inlining modules
+#[derive(Debug)]
+pub enum Error {
+    /// The contents for one or more modules could not be found
+    NotFound(Vec<(String, SourceLocation)>),
+    #[doc(hidden)]
+    __NonExhaustive,
 }
 
 #[cfg(test)]
 mod tests {
-    use std::borrow::Cow;
-    use std::path::Path;
-
     use quote::{quote, ToTokens};
 
-    use crate::{TestResolver, Visitor};
+    use super::*;
 
     fn make_test_env() -> TestResolver {
         let mut env = TestResolver::default();
@@ -49,13 +146,12 @@ mod tests {
     /// Run a full test, exercising the entirety of the functionality in this crate.
     #[test]
     fn happy_path() {
-        let mut visitor = Visitor::<TestResolver>::with_resolver(
-            &Path::new("src/lib.rs"),
-            Cow::Owned(make_test_env()),
-        );
+        let result = InlinerBuilder::default()
+            .parse_internal(Path::new("src/lib.rs"), make_test_env())
+            .unwrap();
 
         assert_eq!(
-            visitor.visit().into_token_stream().to_string(),
+            result.into_token_stream().to_string(),
             quote! {
                 mod first {
                     mod second {
@@ -76,6 +172,33 @@ mod tests {
             }
             .to_string()
         );
+    }
+
+    /// Test case involving a missing module
+    #[test]
+    fn missing_module() {
+        let mut env = TestResolver::default();
+        env.register("src/lib.rs", "mod missing;");
+
+        let result = InlinerBuilder::default()
+            .error_not_found(true)
+            .parse_internal(Path::new("src/lib.rs"), env);
+
+        match result {
+            Err(Error::NotFound(errors)) => {
+                assert_eq!(errors, [(
+                    "missing".into(),
+                    SourceLocation {
+                        path: PathBuf::from("src/lib.rs"),
+                        line: 1,
+                        column: 0,
+                        _private: ()
+                    }
+                )]);
+            }
+            Ok(parsed) => panic!("Expected to get errors in parse/inline: {}", parsed.into_token_stream()),
+            _ => unreachable!()
+        }
     }
 
     /// Test case involving `cfg_attr` from the original request for implementation.
@@ -112,10 +235,12 @@ mod tests {
         );
         env.register("src/empty.rs", "");
 
-        let mut visitor = Visitor::with_resolver(&Path::new("src/lib.rs"), Cow::Borrowed(&env));
+        let result = InlinerBuilder::default()
+            .parse_internal(Path::new("src/lib.rs"), env)
+            .unwrap();
 
         assert_eq!(
-            visitor.visit().into_token_stream().to_string(),
+            result.into_token_stream().to_string(),
             quote! {
                 #[cfg(feature = "m1")]
                 mod m1 {
@@ -167,10 +292,12 @@ mod tests {
         );
         env.register("src/empty.rs", "");
 
-        let mut visitor = Visitor::with_resolver(&Path::new("src/lib.rs"), Cow::Borrowed(&env));
+        let result = InlinerBuilder::default()
+            .parse_internal(Path::new("src/lib.rs"), env)
+            .unwrap();
 
         assert_eq!(
-            visitor.visit().into_token_stream().to_string(),
+            result.into_token_stream().to_string(),
             quote! {
                 #[cfg(feature = "m1")]
                 mod m1 {
--- src/visitor.rs
+++ src/visitor.rs
@@ -4,33 +4,39 @@ use std::path::Path;
 use syn::visit_mut::VisitMut;
 use syn::ItemMod;
 
-use crate::{FileResolver, FsResolver, ModContext};
+use crate::{FileResolver, FsResolver, ModContext, SourceLocation};
 
 pub(crate) struct Visitor<'a, R: Clone> {
     /// The current file's path.
     path: &'a Path,
+    /// Whether this is the root file or not
+    root: bool,
     /// The stack of `mod` entries where the visitor is currently located. This is needed
     /// for cases where modules are declared inside inline modules.
     mod_context: ModContext,
     /// The resolver that can be used to turn paths into `syn::File` instances. This removes
     /// a direct file-system dependency so the expander can be tested.
     resolver: Cow<'a, R>,
+    /// A log of module items that weren't expanded.
+    not_found_log: Option<&'a mut Vec<(String, SourceLocation)>>
 }
 
 impl<'a, R: FileResolver + Default + Clone> Visitor<'a, R> {
     /// Create a new visitor with a default instance of the specified `FileResolver` type.
-    pub fn new(path: &'a Path) -> Self {
-        Self::with_resolver(path, Cow::Owned(R::default()))
+    fn new(path: &'a Path, root: bool, not_found_log: Option<&'a mut Vec<(String, SourceLocation)>>) -> Self {
+        Self::with_resolver(path, root, not_found_log, Cow::Owned(R::default()))
     }
 }
 
 impl<'a, R: FileResolver + Clone> Visitor<'a, R> {
     /// Create a new visitor with the specified `FileResolver` instance. This will be
     /// used by all spawned visitors as we recurse down through the source code.
-    pub fn with_resolver(path: &'a Path, resolver: Cow<'a, R>) -> Self {
+    pub fn with_resolver(path: &'a Path, root: bool, not_found_log: Option<&'a mut Vec<(String, SourceLocation)>>, resolver: Cow<'a, R>) -> Self {
         Self {
             path,
+            root,
             resolver,
+            not_found_log,
             mod_context: Default::default(),
         }
     }
@@ -56,14 +62,16 @@ impl<'a, R: FileResolver + Clone> VisitMut for Visitor<'a, R> {
             // leave the file alone.
             let file = self
                 .mod_context
-                .relative_to(self.path)
+                .relative_to(self.path, self.root)
                 .into_iter()
                 .find(|p| self.resolver.path_exists(&p))
-                .map(|path| Visitor::with_resolver(&path, self.resolver.clone()).visit());
+                .map(|path| Visitor::with_resolver(&path, false, self.not_found_log.as_mut().map(|v|&mut **v), self.resolver.clone()).visit());
 
             if let Some(syn::File { attrs, items, .. }) = file {
                 i.attrs.extend(attrs);
                 i.content = Some((Default::default(), items));
+            } else if let Some(ref mut errors) = self.not_found_log {
+                errors.push((i.ident.to_string(), SourceLocation::new(self.path, i.mod_token.span)))
             }
         }
 
@@ -73,7 +81,7 @@ impl<'a, R: FileResolver + Clone> VisitMut for Visitor<'a, R> {
 
 impl<'a> From<&'a Path> for Visitor<'a, FsResolver> {
     fn from(path: &'a Path) -> Self {
-        Visitor::<FsResolver>::new(path)
+        Visitor::<FsResolver>::new(path, true, None)
     }
 }
 
@@ -89,7 +97,7 @@ mod tests {
     #[test]
     fn ident_in_lib() {
         let path = Path::new("./lib.rs");
-        let mut visitor = Visitor::<PathCommentResolver>::new(&path);
+        let mut visitor = Visitor::<PathCommentResolver>::new(&path, true, None);
         let mut file = syn::parse_file("mod c;").unwrap();
         visitor.visit_file_mut(&mut file);
         assert_eq!(
@@ -106,7 +114,7 @@ mod tests {
     #[test]
     fn path_attr() {
         let path = std::path::Path::new("./lib.rs");
-        let mut visitor = Visitor::<PathCommentResolver>::new(&path);
+        let mut visitor = Visitor::<PathCommentResolver>::new(&path, true, None);
         let mut file = syn::parse_file(r#"#[path = "foo/bar.rs"] mod c;"#).unwrap();
         visitor.visit_file_mut(&mut file);
         assert_eq!(
